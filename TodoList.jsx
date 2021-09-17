a28733c5dd4a4ca3103ad53e99cb1750c835b321
chae-young <cycy8527@gmail.com>
2021-09-17 15:28:24 +0900

    ing

--- TodoList.jsx
+++ TodoList.jsx
@@ -5,6 +5,7 @@ import { ListBox } from "./styles";
 import "antd/dist/antd.css";
 
 const TodoList = () => {
+    const =dddd
     const [text, setText] = useState("");
     const [checklist, setCheckList] = useState([]);
     const [completedCheck, setCompletedCheck] = useState([]);
